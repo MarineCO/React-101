7d75ba65a5ddbfcad0295f1fa2b159c1e8c5ef5a
marineco <marinecolonge@gmail.com>
2017-01-02 22:50:49 +0100

    mise à jour d'un élément ajouté dans le DOM grâce au setInterval qui appelle le DOM chaque seconde

--- public/app.js
+++ public/app.js
@@ -7,12 +7,26 @@ const user = {
 	lastName: 'Colonge'
 };
 
-const element = React.createElement(
-	'h1',
-	null,
-	'Hello, ',
-	formatName(user),
-	'!'
-);
+let tick = () => {
+	const element = React.createElement(
+		'div',
+		null,
+		React.createElement(
+			'h1',
+			null,
+			'Hello, ',
+			formatName(user),
+			'!'
+		),
+		React.createElement(
+			'h2',
+			null,
+			'Il est ',
+			new Date().toLocaleTimeString()
+		)
+	);
 
-ReactDOM.render(element, document.getElementById('test'));
+	ReactDOM.render(element, document.getElementById('test'));
+};
+
+setInterval(tick, 1000);
--- src/app.es6.js
+++ src/app.es6.js
@@ -7,13 +7,18 @@ const user = {
 	lastName: 'Colonge'
 };
 
-const element = 
-	<h1>
-		Hello, {formatName(user)}!
-	</h1>;
+let tick = () => {
+	const element = 
+	<div>
+		<h1>Hello, {formatName(user)}!</h1>
+		<h2>Il est {new Date().toLocaleTimeString()}</h2>
+	</div>
 
 
-ReactDOM.render(
-	element,
-	document.getElementById('test')
-);
+	ReactDOM.render(
+		element,
+		document.getElementById('test')
+	);
+}
+
+setInterval(tick, 1000);
